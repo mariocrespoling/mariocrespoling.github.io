5de5bb9f8775568632cbd67962c59ce7fa80e07c
mariocrespoling <mariocrespoling@gmail.com>
2025-08-20 20:09:25 +0000

    Deploying to gh-pages from @ mariocrespoling/mariocrespoling.github.io@3c84a39e4b47a9c60eff76cd31fc4b368e8054e4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
+        },{id: "nav-about-me",
+          title: "About me",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
